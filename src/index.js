fea34fa46736ba810d3abb4a1dc511e26f1edde4
DJ <dj@intoparty.com>
2021-09-16 00:11:33 +0200

    add rotation, acceleration, grid

--- src/index.js
+++ src/index.js
@@ -2,41 +2,103 @@ import P5 from 'p5'
 
 let p5
 const drawing = new P5(sketch => {p5 = sketch})
-console.log('drawing', drawing)
+console.log('drawing', drawing, '\np5', p5)
 
 const size = 800
-// const heading = p5.createVector(size / 2, size / 2)
-const coordinate = p5.createVector(size / 2, size / 2)
-const heading = p5.createVector((Math.random() * 2) - 1, (Math.random() * 2) - 1)
+const maxSpeed = 4
+let speed = 0
+let acceleration = 0
+const steerStrength = 2
+let angle = 0
+let triangle
 
-function isInBounds (coordinate) {
+const center = p5.createVector(size / 2, size / 2)
+const position = center.copy()
+const target = p5.createVector(Math.random() * size - 1, Math.random() * size - 1)
+// const velocity = p5.createVector(0, 0)
+// const desiredVelocity = p5.createVector(0, 0)
+// const desiredDirection = p5.createVector(0, 0)
+// const desiredSteeringForce = center.copy()
+
+function isInBounds (position) {
   return (
-    coordinate.x < size && coordinate.y < size &&
-    coordinate.x > 0 && coordinate.y > 0
+    position.x < size && position.y < size &&
+    position.x > 0 && position.y > 0
   )
+}
+
+function drawTriangle (position, size = 1) {
+  return p5.triangle(
+    position.x, position.y,
+    position.x - size, position.y + (2 * size),
+    position.x + size, position.y + (2 * size)
+  )
+}
+
+function drawHorizontals () {
+  let x = 0
+
+  while (x < size) {
+    p5.line(0, x, size, x)
+    x += 100
+  }
+}
+
+function drawVerticals () {
+  let y = 0
+
+  while (y < size) {
+    p5.line(y, 0, y, size)
+    y += 100
+  }
+}
+
+function drawGrid () {
+  drawHorizontals()
+  drawVerticals()
 }
 
 p5.setup = () => {
   p5.createCanvas(size, size)
   p5.frameRate(60)
-  p5.background(220)
-  p5.strokeWeight(10)
 
-  // heading.setHeading(Math.random())
-  // heading.normalize()
-  // heading.mult(2)
-  // heading.sub(1, 1)
+  console.log('position', position)
+  console.log('target', target)
 
-  console.log('heading', heading)
-  console.log('coordinate', coordinate)
+  angle = target.copy().sub(position).heading() + (Math.PI / 2)
+
+  console.log('angle in radians', angle.toFixed(4))
+  console.log('angle in degrees', p5.degrees(angle).toFixed(4))
 }
 
 p5.draw = () => {
-  p5.background(220)
-  p5.point(coordinate)
+  const difference = target.copy().sub(position)
 
-  if (isInBounds(coordinate)) coordinate.add(heading)
-  else {
+  if (speed <= maxSpeed) {
+    speed += acceleration
+    acceleration += 0.001
+  }
+
+  const distance = position.copy().dist(target)
+
+  const translation = difference.normalize().mult(speed).limit(distance)
+
+  p5.background(220)
+  p5.strokeWeight(10)
+  p5.point(target)
+  p5.point(center)
+  p5.strokeWeight(1)
+
+  if (isInBounds(position) && position !== target) {
+    p5.translate(position)
+    p5.rotate(angle)
+    drawGrid()
+
+    drawTriangle(p5.createVector(0, 0), 10)
+    position.add(translation)
+    // p5.translate(0, 0)
+
+  } else {
     console.log('done')
     p5.noLoop()
   }
